10388d0a5569d34d6b4a31f74089c0c288721c88
jr0998 <33519065+jr0998@users.noreply.github.com>
2021-12-03 10:29:20 -0500

    added images

--- src/components/DetectImage.vue
+++ src/components/DetectImage.vue
@@ -9,6 +9,19 @@
       >.
     </p>
     <h3>Installed CLI Plugins</h3>
+    <div>
+    <h1>{{ msg }}</h1>
+    <div>
+        <img class="image" ref="img" :src="require('../assets/images/' + getImgIndex + '.jpg')" />
+    </div>
+    <div>
+        <button class="button" @click="next()" :disabled="disable">Next</button>
+    </div>
+    <div v-for="pred in predictions" :key="pred.index">
+        {{ pred.label }}: {{ pred.probability.toFixed(0) + '%' }}
+    </div>
+    <div v-if="!predictions.length">hmm.....</div>
+</div>
     <ul>
       <li>
         <a
@@ -100,6 +113,29 @@ export default {
   props: {
     msg: String,
   },
+  data() {
+    return {
+        image: 0,
+        numImages: 16,
+    };
+  },
+computed: {
+    getImgIndex() {
+        return this.image.toString();
+    },
+    disable() {
+        if (this.image == this.numImages) {
+            return true;
+        } else return false;
+    },
+},
+methods: {
+    next() {
+        this.image++;
+        this.predictions = [];
+        setTimeout(this.predict, 500);
+    },
+}
 };
 </script>
 
@@ -118,5 +154,29 @@ li {
 }
 a {
   color: #42b983;
+}
+<style scoped>
+h3 {
+    margin: 40px 0 0;
+}
+.image {
+    min-height: 300px;
+    max-height: 300px;
+    max-width: 100%;
+}
+.button {
+    width: 200px;
+    height: 50px;
+    border-radius: 5px;
+    background-color: blueviolet;
+    color: white;
+    font-size: 20pt;
+    margin: 10px;
+}
+.button:disabled,
+.button[disabled] {
+    border: 1px solid #999999;
+    background-color: #cccccc;
+    color: #666666;
 }
 </style>
